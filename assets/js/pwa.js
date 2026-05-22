39e1e0905eff2ae7e597350392859ba03e4cf4b6
tpark2142-cloud <tpark2142@gmail.com>
2026-05-21 21:20:25 -0600

    Add PWA app setup with new app icon

---
+++ assets/js/pwa.js
@@ -0,0 +1,8 @@
+(function(){
+  if(!('serviceWorker' in navigator)){
+    return;
+  }
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('service-worker.js').catch(() => {});
+  });
+})();
---
+++ service-worker.js
@@ -0,0 +1,46 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('lifemoment-static-v1').then(cache => cache.addAll([
+      './',
+      './index.html',
+      './devotion.html',
+      './prayer.html',
+      './comfort.html',
+      './manifest.json',
+      './assets/css/app.css',
+      './assets/js/app.js',
+      './assets/js/lang.js',
+      './assets/js/data.js',
+      './assets/js/ai-assistant.js',
+      './assets/img/app-icon.png'
+    ])).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys
+        .filter(key => key !== 'lifemoment-static-v1')
+        .map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) {
+        return cached;
+      }
+      return fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open('lifemoment-static-v1').then(cache => cache.put(event.request, copy));
+        return response;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
