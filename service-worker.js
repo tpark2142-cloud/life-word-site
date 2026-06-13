b88ee503435129cba252f2b30decd1e79e85cd42
tpark2142-cloud <tpark2142@gmail.com>
2026-06-12 23:10:24 -0600

    Add third Korean video to video library

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 ﻿self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('lifemoment-static-v88').then(cache => cache.addAll([
+    caches.open('lifemoment-static-v89').then(cache => cache.addAll([
       './',
       './index.html',
       './robots.txt',
@@ -30,7 +30,7 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys
-        .filter(key => key !== 'lifemoment-static-v88')
+        .filter(key => key !== 'lifemoment-static-v89')
         .map(key => caches.delete(key))
     )).then(() => self.clients.claim())
   );
@@ -47,7 +47,7 @@ self.addEventListener('fetch', event => {
       }
       return fetch(event.request).then(response => {
         const copy = response.clone();
-        caches.open('lifemoment-static-v88').then(cache => cache.put(event.request, copy));
+        caches.open('lifemoment-static-v89').then(cache => cache.put(event.request, copy));
         return response;
       }).catch(() => caches.match('./index.html'));
     })
