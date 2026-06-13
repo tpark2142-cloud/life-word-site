4cdbd410b564c6843be632fa8bf8f806a47c4e9e
tpark2142-cloud <tpark2142@gmail.com>
2026-06-12 23:07:11 -0600

    Add Korean video library section with new YouTube video

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 ﻿self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('lifemoment-static-v87').then(cache => cache.addAll([
+    caches.open('lifemoment-static-v88').then(cache => cache.addAll([
       './',
       './index.html',
       './robots.txt',
@@ -15,7 +15,7 @@
       './bible-verses-for-healing.html',
       './prayers-for-healing.html',
       './manifest.json',
-      './assets/css/app.css?v=20260608b',
+      './assets/css/app.css?v=20260612a',
       './assets/js/app.js?v=20260608b',
       './assets/js/lang.js?v=20260604a',
       './assets/js/pwa.js?v=20260606b',
@@ -30,7 +30,7 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys
-        .filter(key => key !== 'lifemoment-static-v87')
+        .filter(key => key !== 'lifemoment-static-v88')
         .map(key => caches.delete(key))
     )).then(() => self.clients.claim())
   );
@@ -47,7 +47,7 @@ self.addEventListener('fetch', event => {
       }
       return fetch(event.request).then(response => {
         const copy = response.clone();
-        caches.open('lifemoment-static-v87').then(cache => cache.put(event.request, copy));
+        caches.open('lifemoment-static-v88').then(cache => cache.put(event.request, copy));
         return response;
       }).catch(() => caches.match('./index.html'));
     })
