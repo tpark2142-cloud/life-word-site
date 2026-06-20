407e159da493983dee29040d2b63ea3711625b4e
tpark2142-cloud <tpark2142@gmail.com>
2026-06-20 09:38:32 -0600

    Remove nested navigation from embedded SEO pages

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 ﻿self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('lifemoment-static-v102').then(cache => cache.addAll([
+    caches.open('lifemoment-static-v103').then(cache => cache.addAll([
       './',
       './index.html',
       './robots.txt',
@@ -39,7 +39,7 @@
       './ko/prayer-for-sick-family.html',
       './manifest.json',
       './assets/css/app.css?v=20260615b',
-      './assets/css/seo-pages.css?v=20260619a',
+      './assets/css/seo-pages.css?v=20260620a',
       './assets/js/app.js?v=20260615a',
       './assets/js/lang.js?v=20260604a',
       './assets/js/pwa.js?v=20260606b',
@@ -54,7 +54,7 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys
-        .filter(key => key !== 'lifemoment-static-v102')
+        .filter(key => key !== 'lifemoment-static-v103')
         .map(key => caches.delete(key))
     )).then(() => self.clients.claim())
   );
@@ -71,7 +71,7 @@ self.addEventListener('fetch', event => {
       }
       return fetch(event.request).then(response => {
         const copy = response.clone();
-        caches.open('lifemoment-static-v102').then(cache => cache.put(event.request, copy));
+        caches.open('lifemoment-static-v103').then(cache => cache.put(event.request, copy));
         return response;
       }).catch(() => caches.match('./index.html'));
     })
