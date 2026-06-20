db156517eb1d0e3622b0c63086ed605b4d8eabe6
tpark2142-cloud <tpark2142@gmail.com>
2026-06-20 10:11:15 -0600

    Remove duplicate footer links from SEO pages

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 ﻿self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('lifemoment-static-v106').then(cache => cache.addAll([
+    caches.open('lifemoment-static-v107').then(cache => cache.addAll([
       './',
       './index.html',
       './robots.txt',
@@ -54,7 +54,7 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys
-        .filter(key => key !== 'lifemoment-static-v106')
+        .filter(key => key !== 'lifemoment-static-v107')
         .map(key => caches.delete(key))
     )).then(() => self.clients.claim())
   );
@@ -71,7 +71,7 @@ self.addEventListener('fetch', event => {
       }
       return fetch(event.request).then(response => {
         const copy = response.clone();
-        caches.open('lifemoment-static-v106').then(cache => cache.put(event.request, copy));
+        caches.open('lifemoment-static-v107').then(cache => cache.put(event.request, copy));
         return response;
       }).catch(() => caches.match('./index.html'));
     })
